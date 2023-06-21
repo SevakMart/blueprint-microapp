9a525738a019b40d7251663b3164b1bc0c0e0ed5
Sevak Martirosyan <epam_smartirosyan@exactsciences.com>
2023-06-21 15:38:13 +0400

    add template

---
+++ server/src/main/java/com/microapp/server/controllers/ClientForwardController.java
@@ -0,0 +1,14 @@
+package com.microapp.server.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class ClientForwardController {
+    
+    @GetMapping(value = "/**/{path:[^\\.]*}")
+    public String forward(@PathVariable String path) {
+        return "forward:/";
+    }
+}
